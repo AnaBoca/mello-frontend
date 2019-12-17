252e9c54ae3232370821351d5a5ce7d40c252122
Ana Boca <Ana.Boca@asu.edu>
2019-12-17 22:42:51 +0000

    functional static application

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,12 +1,16 @@
 class Card {
   constructor(text) {
     this.text = text;
+    this.id = ++Card.lastId;
   }
 }
+
+Card.lastId = 0;
 
 class List {
   constructor(title) {
     this.title = title;
+    this.id = ++List.lastId;
     this.cards = [];
   }
 
@@ -16,8 +20,11 @@ class List {
   }
 }
 
+List.lastId = 0;
+
 class Board {
   constructor() {
+    this.id = ++Board.lastId;
     this.lists = [];
   }
 
@@ -25,4 +32,26 @@ class Board {
     var list = new List(text);
     this.lists.push(list);
   }
+
+  findList(listId) {
+    return this.lists.find(function(list) {
+      return listId === list.id;
+    });
+  }
+
+  editList(listId, newTitle) {
+    var list = this.findList(listId);
+    if (list) {
+      list.title = newTitle;
+    }
+  }
+
+  addCard(listId, cardText) {
+    var list = this.findList(listId);
+    if (list) {
+      list.addCard(cardText);
+    }
+  }
 }
+
+Board.lastId = 0;
